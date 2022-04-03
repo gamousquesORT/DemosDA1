e8a8220a9ee6940b16120cb350f4a62ae11c8190
gamousquesORT <mousques@ort.edu.uy>
2022-04-03 14:57:27 -0300

    Commit inicial con estructura de proyectos en solución

---
+++ TDD/practica/FizzBuzzDemo/TestFizzBuzz/UnitTest1.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+/*
+ *
+ * Estructura de los test
+        Arrange
+        Act
+        Assert
+ *
+ * Reglas FizzBuzz
+ * Crear una clase que tenga un método que dado un número retorne:
+        Fizz si el número es divisible por 3
+        Buzz si el número es divisible por 5
+        FizzBuzz si el número es divisible por 3 y 5
+        Si el número no es divisible por 3 ni 5 retorna el número 
+ * 
+ */
+namespace TestFizzBuzz
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
