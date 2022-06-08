b8b795dbaadc50493a55cbe4a87f7913c585d7ee
gamousquesORT <mousques@ort.edu.uy>
2022-06-08 19:12:39 -0300

    added factory example

---
+++ Patrones/SingletonClassicCounter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SingletonClassicCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Valor inicial {0}", GlobalCounter.Instance.Counter());
+            GlobalCounter.Instance.Increment();
+            Console.WriteLine("Valor inicial {0}", GlobalCounter.Instance.Counter());
+            GlobalCounter.Instance.Increment();
+            Console.WriteLine("Valor inicial {0}", GlobalCounter.Instance.Counter());
+
+        }
+    }
+}
---
+++ Patrones/SingletonClassicCounter/SingletonClassicCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClassicCounter
+{
+    // esta es la clase de la que queremos tener una unica instancia
+    public class GlobalCounter
+    {
+        // aca vamos a guardar la unica instancia 
+        // de esta clase. Notar que es un atributo de clase
+        private static GlobalCounter _instance;
+
+        private int _counter;
+        
+        // Hacer el constructor privado: me asegura que nadie mas puede crear instancias
+        private GlobalCounter() {
+            // ....
+            _instance = null;
+        }
+
+        public static GlobalCounter Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new GlobalCounter();
+                }
+                return _instance;
+            }
+        }
+
+        // ....
+        // logica propia de sistema. En este ejemplo, solo llevar un contador
+        
+        public void Increment() {
+            this._counter++;
+        }
+
+        public int Counter() {
+            return this._counter;
+        }
+    }
+}
