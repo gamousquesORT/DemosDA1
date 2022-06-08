bfb1898d2d1f129a4e91082e6f4c105ada057ab5
gamousquesORT <mousques@ort.edu.uy>
2022-06-08 14:24:09 -0300

    version inicial 2021

---
+++ Patrones/EjemplosDePatrones/EjemploSingleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemplosDePatrones
+{
+    // esta es la clase de la que queremos tener una unica instancia
+    public class Sistema
+    {
+        // aca vamos a guardar la unica instancia 
+        // de esta clase. Notar que es un atributo de clase
+        private static Sistema instancia;
+
+        // Hacer el constructor privado: me asegura que nadie mas puede crear instancias
+        private Sistema() {
+            // ....
+        }
+
+        public static Sistema Instancia {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new Sistema();
+                }
+                return instancia;
+            }
+        }
+
+        // ....
+        // logica propia de sistema. En este ejemplo, solo llevar un contador
+
+        private int contador;
+
+        public void Incrementar() {
+            this.contador++;
+        }
+
+        public int GetValor() {
+            return this.contador;
+        }
+    }
+}
