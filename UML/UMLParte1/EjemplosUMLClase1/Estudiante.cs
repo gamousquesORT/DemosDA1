37d6c292aac906e0588639b6e8494d7b42f89a70
gamousquesORT <mousques@ort.edu.uy>
2022-08-18 14:43:17 -0300

    added Estudiante

---
+++ UML/UMLParte1/EjemplosUMLClase1/Estudiante.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Sockets;
+
+namespace ejemplosClase
+{
+    public class Estudiante
+    {
+        private long _numeroEstudiante;
+        private string _nombre;
+        private DateTime _fechaIngreso;
+        private static long _proximoNumeroEstudiante = 0;
+
+        static Estudiante()
+        {
+            _proximoNumeroEstudiante = 1;
+        }
+        public Estudiante()
+        {
+            _numeroEstudiante += _proximoNumeroEstudiante;
+        }
+
+        public long NumeroEstudiante
+        {
+            get { return _numeroEstudiante; }
+        }
+
+        public DateTime FechaIngreso
+        {
+            set { _fechaIngreso = value; }
+            get { return _fechaIngreso; }
+        }
+        public string Nombre
+        {
+            get => _nombre;
+            set => _nombre = value ?? throw new ArgumentNullException(nameof(value));
+        }
+    }
+}
