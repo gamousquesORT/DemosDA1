ccafe26703df47d9be230d35be978de991828bd3
gamousquesORT <mousques@ort.edu.uy>
2022-08-18 17:07:45 -0300

    refactor proyectos

---
+++ UML/UMLParte1/CountryCapital/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ejemplosClase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country uy = new Country("Uruguay", "Montevideo");
+            Console.WriteLine("preguntando al pais los datos --> Pais: {0} Capital {1} ", uy.Name, uy.CapitalName);
+
+            Capital capital = uy.Capital;
+            Console.WriteLine("Preguntando al pais y la a la capital -->Pais: {0} Capital {1} ", uy.Name,capital.Name);
+             
+        }
+    }
+}
--- UML/UMLParte1/EjemplosUMLClase1/Program.cs
+++ UML/UMLParte1/EjemplosUMLClase1/Program.cs
@@ -8,12 +8,6 @@ namespace ejemplosClase
  
         static void Main(string[] args)
         { 
-            Country uy = new Country("Uruguay", "Montevideo");
-            Console.WriteLine("preguntando al pais los datos --> Pais: {0} Capital {1} ", uy.Name, uy.CapitalName);
-
-            Capital capital = uy.Capital;
-            Console.WriteLine("Preguntando al pais y la a la capital -->Pais: {0} Capital {1} ", uy.Name,capital.Name);
-             
 
          
           
