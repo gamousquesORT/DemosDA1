86b3f16b91b8c0c8aabf84ba7f1eda6776903191
gamousquesORT <mousques@ort.edu.uy>
2022-09-11 13:35:28 -0300

    added exception test

--- TDD/practica2022_2/FizzBuzz/FizzBuzzLogic/FizzBuzz.cs
+++ TDD/practica2022_2/FizzBuzz/FizzBuzzLogic/FizzBuzz.cs
@@ -1,25 +1,30 @@
-namespace FizzBuzzLogic
-{
+namespace FizzBuzzLogic;
 
-    public class FizzBuzz
+public class FizzBuzz
+{
+    private const string Fizz = "Fizz";
+    private const string Buzz = "Buzz";
+    private const string fizzBuzz = "FizzBuzz";
+    public String GetFizzBuzz(int number)
     {
-        private const string fizz = "Fizz";
-        private const string buzz = "Buzz";
-        private const string fizzBuzz = "FizzBuzz";
-        public String GetFizzBuzz(int number)
+        if (number <= 100)
         {
             if (number == 3)
-                return fizz;
+                return Fizz;
             else if (number == 5)
-                return buzz;
+                return Buzz;
             else if (ModuleOfNumber(number))
                 return fizzBuzz;
             return number.ToString();
         }
-
-        private static bool ModuleOfNumber(int number)
+        else
         {
-            return number % 5 == 0 || number % 3 == 0;
+            throw new ArgumentOutOfRangeException("el valor es mayor a 100");
         }
+    }
+
+    private static bool ModuleOfNumber(int number)
+    {
+        return number % 5 == 0 || number % 3 == 0;
     }
 }
--- TDD/practica2022_2/FizzBuzz/TestFizzBuzz/TestFizzBuzz/FizzBuzzTest.cs
+++ TDD/practica2022_2/FizzBuzz/TestFizzBuzz/TestFizzBuzz/FizzBuzzTest.cs
@@ -1,7 +1,6 @@
 using FizzBuzzLogic;
 
-namespace TestFizzBuzzTest
-{
+namespace TestFizzBuzzTest;
 
 /*
  * For multiples of three print Fizz instead of the number
@@ -9,46 +8,52 @@ namespace TestFizzBuzzTest
  * For numbers which are multiples of both three and five print FizzBuzzLogic instead of the number
  */
 
-    public class FizzBuzzTest
+public class FizzBuzzTest
+{
+    [Fact]
+    public void ShouldReturn_Fizz_for_3()
     {
-        [Fact]
-        public void ShouldReturn_Fizz_for_3()
-        {
-            var fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetFizzBuzz(3);
-            Assert.Equal("Fizz", result);
-        }
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz(3);
+        Assert.Equal("Fizz", result);
+    }
         
-        [Fact]
-        public void ShouldReturn_Buzz_for_5()
-        {
-            var fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetFizzBuzz(5);
-            Assert.Equal("Buzz", result);
-        }
+    [Fact]
+    public void ShouldReturn_Buzz_for_5()
+    {
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz(5);
+        Assert.Equal("Buzz", result);
+    }
         
-        [Fact]
-        public void ShouldReturn_FizzBuzz_for_15()
-        {
-            var fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetFizzBuzz(15);
-            Assert.Equal("FizzBuzz", result);
-        }
+    [Fact]
+    public void ShouldReturn_FizzBuzz_for_15()
+    {
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz(15);
+        Assert.Equal("FizzBuzz", result);
+    }
         
-        [Fact]
-        public void ShouldReturn_1_for_1()
-        {
-            var fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetFizzBuzz(1);
-            Assert.Equal("1", result);
-        }
+    [Fact]
+    public void ShouldReturn_1_for_1()
+    {
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz(1);
+        Assert.Equal("1", result);
+    }
 
-        [Fact]
-        public void ShouldReturn_FizzBuzz_for_30()
-        {
-            var fizzBuzz = new FizzBuzz();
-            var result = fizzBuzz.GetFizzBuzz(30);
-            Assert.Equal("FizzBuzz", result);
-        }
+    [Fact]
+    public void ShouldReturn_FizzBuzz_for_30()
+    {
+        var fizzBuzz = new FizzBuzz();
+        var result = fizzBuzz.GetFizzBuzz(30);
+        Assert.Equal("FizzBuzz", result);
+    }
+
+    [Fact]
+    public void ShouldReturnRangeException_for_101()
+    {
+        var fizzBuzz = new FizzBuzz();
+        Assert.Throws<ArgumentOutOfRangeException>(() => fizzBuzz.GetFizzBuzz(101));
     }
 }
