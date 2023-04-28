3f290ead796d1257b6ad19b1a8a5587220a3220c
gamousquesORT <mousques@ort.edu.uy>
2023-04-28 08:04:11 -0300

    added equality ops

--- CleanCode/Ejemplos/WithDemeter/Customer.cs
+++ CleanCode/Ejemplos/WithDemeter/Customer.cs
@@ -1,13 +1,17 @@
 namespace WithDemeter;
 
-public class Customer
+public class Customer : IEquatable<Customer>
 {
     private Wallet _wallet =new Wallet();
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
     public void AddMoney(decimal amount)
     {
         _wallet.AddMoney(amount);
+    }
+
+    public Customer()
+    {
     }
 
     public bool Pay(decimal amountToPay)
@@ -19,5 +23,38 @@ public class Customer
         }
 
         return false;
+    }
+
+    public bool Equals(Customer? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return FirstName == other.FirstName && LastName == other.LastName;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Customer) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            return (FirstName.GetHashCode() * 397) ^ LastName.GetHashCode();
+        }
+    }
+
+    public static bool operator ==(Customer? left, Customer? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Customer? left, Customer? right)
+    {
+        return !Equals(left, right);
     }
 }
--- CleanCode/Ejemplos/WithDemeter/Wallet.cs
+++ CleanCode/Ejemplos/WithDemeter/Wallet.cs
@@ -1,6 +1,6 @@
 namespace WithDemeter;
 
-public class Wallet
+public class Wallet : IEquatable<Wallet>
 {
     public decimal Money { get; set; } = 0;
 
@@ -11,5 +11,35 @@ public class Wallet
     public void DrawMoney(decimal money)
     {
         Money -= money;
+    }
+
+    public bool Equals(Wallet? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Money == other.Money;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Wallet) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return Money.GetHashCode();
+    }
+
+    public static bool operator ==(Wallet? left, Wallet? right)
+    {
+        return Equals(left, right);
+    }
+
+    public static bool operator !=(Wallet? left, Wallet? right)
+    {
+        return !Equals(left, right);
     }
 }
