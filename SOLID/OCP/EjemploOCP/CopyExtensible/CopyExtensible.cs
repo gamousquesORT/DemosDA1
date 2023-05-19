9dd2f66f9b754862e1443458050c28558077c14d
gamousquesORT <mousques@ort.edu.uy>
2023-05-19 15:46:37 -0300

    Added COP refactred

---
+++ SOLID/OCP/EjemploOCP/CopyExtensible/CopyExtensible.cs
@@ -0,0 +1,51 @@
+﻿namespace CopyExtensible;
+
+public class CopyExtensible
+{
+    private IInput _input;
+    private IOutput _output;
+
+    public CopyExtensible(IInput input, IOutput output)
+    {
+        _input = input;
+        _output = output;
+    }
+
+    public void CopyStrings()
+    {
+            string inputValue;
+
+            while ((inputValue = _input.Read()) != "")
+                _output.Write(inputValue);
+    }
+}
+
+
+public interface IInput
+{
+    string Read();
+}
+
+public interface IOutput
+{
+    void Write(string outputValue);
+}
+
+
+public class KeyBoardReader : IInput
+{
+    public string Read()
+    {
+        Console.Write("->");
+        return Console.ReadLine();
+    }
+}
+
+public class MonitorOutput : IOutput
+{
+    public void Write(string outputValue)
+    {
+        Console.Write("<-");
+        Console.WriteLine(outputValue);
+    }
+}
---
+++ SOLID/OCP/EjemploOCP/CopyExtensible/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CopyExtensible;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IInput inputObject = new KeyBoardReader();
+        IOutput outputObject = new MonitorOutput();
+        
+        CopyExtensible copyUtil = new CopyExtensible(inputObject, outputObject);
+        copyUtil.CopyStrings();
+    }
+}
