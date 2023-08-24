a6ad80d466901ff281520a5ef18d05a15b2f4bd2
gamousquesORT <mousques@ort.edu.uy>
2023-08-24 15:15:09 -0300

    added new exercises on inheritance and polymorphism

---
+++ UML/UMLParte2/codigoParaDiagramar/EjericiosHerenciaDiagramar/EjercicioPolimorfismoES/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace EjericiosPolimorfismo {
+    class Program {
+        static void Main(string[] args) {
+            // Usage
+            Car car = new Car { Make = "Fiat", Model = "580", NumDoors = 4 };
+            Motorcycle motorcycle = new Motorcycle { Make = "Harley-Davidson", Model = "Sportster", HasSidecar = false };
+
+            Console.WriteLine("----- --------");
+            Console.WriteLine("1- Maker:" + car.Make); 
+            Console.WriteLine("2- Model:" + car.Model); 
+            Console.WriteLine("3- Doors: " + car.NumDoors);
+            car.Drive();          // Output: ??
+            
+            Console.WriteLine("-------------");
+            Console.WriteLine("4- Maker:" + motorcycle.Make); 
+            Console.WriteLine("5- Model:" + motorcycle.Model); 
+            Console.WriteLine("6 - Sidecar:" + motorcycle.HasSidecar); 
+            motorcycle.Drive();          // Output: ??
+            Console.WriteLine("-------------");
+
+            Vehicle newCar = new Car { Make = "Fiat", Model = "580", NumDoors = 4};
+            Vehicle newMotorcycle = new Motorcycle { Make = "Harley-Davidson", Model = "Sportster", HasSidecar = false };
+
+            Console.WriteLine("7- Maker:" + newCar.Make);
+            Console.WriteLine("8- Model:" + newCar.Model);
+            //Console.WriteLine("9- Doors: " + newCar.NumDoors); 
+            newCar.Drive();          // Output: ??
+            
+            Console.WriteLine("-------------");
+            Console.WriteLine("10- Maker:" + newMotorcycle.Make); 
+            Console.WriteLine("11- Model:" + newMotorcycle.Model); // Output: ??
+            //Console.WriteLine("12- Sidecar:" + newMotorcycle.HasSidecar); 
+            newMotorcycle.Drive();          // Output: ??
+            Console.WriteLine("-------------");
+
+        }
+    }
+}
---
+++ UML/UMLParte2/codigoParaDiagramar/EjericiosHerenciaDiagramar/EjercicioPolimorfismoES/Vehicles.cs
@@ -0,0 +1,52 @@
+﻿namespace EjericiosPolimorfismo
+{
+    public class Vehicle
+    {
+        public Vehicle() {}
+        public Vehicle(string make, string model)
+        {
+            Make = make;
+            Model = model;
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public void Drive()
+        {
+            Console.WriteLine("Vehicle is running.");
+        }
+        
+    }
+
+    public class Car : Vehicle
+    {
+        public Car():base() {}
+        public Car(string make, string model, int numDoors) : base(make, model)
+        {
+            NumDoors = numDoors;
+        }
+        public int NumDoors { get; set; }
+        public void Drive()
+        {
+            Console.WriteLine("Car is running.");
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle():base() {}
+        public Motorcycle(string make, string model, bool hasSidecar) : base(make, model)
+        {
+            HasSidecar = hasSidecar;
+        }
+
+        public bool HasSidecar { get; set; }
+        public void Drive()
+        {
+            Console.WriteLine("Motorcycle is running.");
+        }
+    }  
+    
+}
+
