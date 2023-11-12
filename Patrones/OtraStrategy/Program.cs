139e00ab2c3c519a7d0e400bc9474dcaf34672b3
gamousquesORT <mousques@ort.edu.uy>
2023-11-12 16:50:10 -0300

    changed solution to net7

---
+++ Patrones/OtraStrategy/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EjemplosDePatrones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pais us = new USA();
+            Pais uy = new Uruguay();
+
+            Compra c = new Compra() {Total = 100, Pais = uy};
+            Console.WriteLine($"Impuestos en UY: {c.CalcularImpuestos()}");
+
+            c.Pais = us;
+            Console.WriteLine($"Impuestos en USA: {c.CalcularImpuestos()}");
+
+            Console.ReadLine();
+
+        }
+    }
+    
+}
