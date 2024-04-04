215fcccb8d98601020b7f8a537674e7042fe9829
Gastón Mousqués <mousques@ort.edu.uy>
2024-04-04 20:10:31 -0300

    clean up folder

--- TDD/practica2024_1/FizzBuzzTests/FizzBuzzTest.cs
+++ TDD/practica2024_1/FizzBuzzTests/FizzBuzzTest.cs
@@ -16,17 +16,17 @@ namespace FizzBuzzTests;
         Si el número no es divisible por 3 ni 5 retorna el número
         Si el número es negativo lance una excepción 
  *
- * DEfinir tests
+ * Definir tests
  *      1. Se llama a la funcion para ver que no exista la clase y todo funciona
  *      2. Dado 3 retorna Fizz
  *      3. Dado 5 retorna Buzz
  *      4. Dado 2 retorna "2"
- *      5.Se pasa un número divisible por 3 y retorna Fizz
- *      6. Se pasa un número divisible por 5 y retorna Buzz
- *      7. se pasa un numero divisble por 3 y 5 y retorna FizzBuzzLogic
- *      8. Se pasa un número no divisble por 3 y retorna el numero como string
- *      9. se pasa cero FizzBuzz
- *      10. se pasa un número negativo y retorna excepción
+ *      5. Dado un número divisible por 3 y retorna Fizz
+ *      6. Dado un número divisible por 5 y retorna Buzz
+ *      7. Dado un numero divisble por 3 y 5 y retorna FizzBuzzLogic
+ *      8. Dado un número no divisble por 3 y retorna el numero como string
+ *      9. Dado cero FizzBuzz
+ *      10. Dado un número negativo y retorna excepción
  *
  */
 
