84d64a6694efb80d59bca10aba9c595b45fe19f1
Gastón Mousqués <mousques@ort.edu.uy>
2024-11-11 06:10:38 -0300

    Actualizar OCPCompliantClass.cs
    Fixed comment typos

--- SOLID/EjemploOCPFabricaMethod/OCPFileImporter/OCPCompliantClass.cs
+++ SOLID/EjemploOCPFabricaMethod/OCPFileImporter/OCPCompliantClass.cs
@@ -11,8 +11,8 @@ public class OCPCompliantClass
     }
     public string ImportFile(string fileName)
     {
-        // esta metodos puede ser mucho mas complejo. lo importante es notar que 
-        // no tiene código de importanción de archivos, eso se refactoreo a la clase FileImporter
+        // este método puede ser mucho mas complejo. lo importante es notar que 
+        // no tiene código de importación de archivos, eso se refactoreo a la clase FileImporter
         
         return _importer.ImportFile(fileName);
     }
